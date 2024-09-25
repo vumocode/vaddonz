0fe25df513bd1be03b96a21fb43db9f9ddd5e8c2
vumocode <91375898+vumocode@users.noreply.github.com>
2024-09-25 21:15:17 +0200

    yo

---
+++ install/hod.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         HEROES TO DISCORD
+// @version      1.0
+// @description  Dodatek umozliwiający wysyłanie (automatycznie, bądź nie) pingów o herosach, kolosach, tytanach na discorda.
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/vaddonz/addons/hod.js
+// @updateURL    https://vumocode.github.io/vaddonz/addons/hod.js
+// @require      https://vumocode.github.io/vaddonz/addons/hod.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
--- install/podpisy.user.js
+++ install/podpisy.user.js
@@ -4,9 +4,9 @@
 // @description  Podpisy KCS'ów
 // @author       vumo
 // @match        https://*.margonem.pl/
-// @downloadURL  https://vumocode.github.io/margonem-addons/addons/podpisy.js
-// @updateURL    https://vumocode.github.io/margonem-addons/addons/podpisy.js
-// @require      https://vumocode.github.io/margonem-addons/addons/podpisy.js
+// @downloadURL  https://vumocode.github.io/vaddonz/addons/podpisy.js
+// @updateURL    https://vumocode.github.io/vaddonz/addons/podpisy.js
+// @require      https://vumocode.github.io/vaddonz/addons/podpisy.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
 // @grant        none
 // ==/UserScript==
