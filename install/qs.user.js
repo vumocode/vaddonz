9ef5e5703f7db0b4d9b8ed3e2294e3251301ec2a
vumocode <91375898+vumocode@users.noreply.github.com>
2024-09-27 00:31:06 +0200

    Create qs.user.js

---
+++ install/qs.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Quick Sell
+// @version      1.0
+// @description  Sprzedawanie pod 1,2,3 enter
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @downloadURL  https://vumocode.github.io/vaddonz/addons/qs.js
+// @updateURL    https://vumocode.github.io/vaddonz/addons/qs.js
+// @require      https://vumocode.github.io/vaddonz/addons/qs.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
