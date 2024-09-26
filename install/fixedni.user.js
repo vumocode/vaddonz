53683a214d995bd4d0d522fd2e9e08197f948f87
vumocode <91375898+vumocode@users.noreply.github.com>
2024-09-27 01:26:16 +0200

    aaa

--- install/fixedni.user.js
+++ install/fixedni.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         BETTER LOOTLOG
+// @name         FIXED NI
 // @version      1.0
 // @description  Poprawiony dodatek DARKNESS NI.
 // @description  [ autorstwa Xaller i Zeph ]
 // @author       vumo
 // @match        https://*.margonem.pl/
-// @downloadURL  https://vumocode.github.io/vaddonz/addons/darkni.js
-// @updateURL    https://vumocode.github.io/vaddonz/addons/darkni.js
-// @require      https://vumocode.github.io/vaddonz/addons/darkni.js
+// @downloadURL  https://vumocode.github.io/vaddonz/addons/fixedni.js
+// @updateURL    https://vumocode.github.io/vaddonz/addons/fixedni.js
+// @require      https://vumocode.github.io/vaddonz/addons/fixedni.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
 // @grant        none
 // ==/UserScript==
