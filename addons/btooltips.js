b6e4d9d5d82f5332db78d12de584aa9c1e0e6f46
vumocode <91375898+vumocode@users.noreply.github.com>
2024-09-27 10:26:02 +0200

    f

--- addons/btooltips.js
+++ addons/btooltips.js
@@ -4,6 +4,7 @@
 // @description  Dodatek poprawiający widok tooltipów.
 // @author       vumo
 // @match        https://*.margonem.pl/
+// @match        https://www.margonem.pl/*
 // @downloadURL  https://vumocode.github.io/vaddonz/addons/btooltips.js
 // @updateURL    https://vumocode.github.io/vaddonz/addons/btooltips.js
 // @require      https://vumocode.github.io/vaddonz/addons/btooltips.js
