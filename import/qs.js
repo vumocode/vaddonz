c010b85f934f7fa3606003baf38b44a2620786f4
vumocode <91375898+vumocode@users.noreply.github.com>
2024-10-07 04:27:21 +0200

    Create qs.js

---
+++ import/qs.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         QUICK SELL
+// @version      1.0
+// @description  Sprzedawanie pod przyciskami (1, 2, 3, enter)
+// @author       vumo
+// @match        https://*.margonem.pl/
+// @exclude      https://www.margonem.pl/*
+// @icon         https://vumocode.github.io/vaddonz/dependencies/assets/vaddonz.ico
+// @grant        none
+// ==/UserScript==
+
+document.addEventListener('keydown', function(event) {
+    let buttonToClick = null;
+
+    if (event.key === '1') {
+        buttonToClick = document.querySelector('.button.green.small.btn-num');
+    } else if (event.key === '2') {
+        buttonToClick = document.querySelectorAll('.button.green.small.btn-num')[1];
+    } else if (event.key === '3') {
+        buttonToClick = document.querySelectorAll('.button.green.small.btn-num')[2];
+    } else if (event.key === 'Enter') {
+        buttonToClick = document.querySelector('.finalize-button.btns-spacing .button.green.small');
+    }
+
+    if (buttonToClick && buttonToClick.offsetParent !== null) {
+        event.preventDefault();
+        event.stopPropagation();
+        buttonToClick.click();
+    }
+});
