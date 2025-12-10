d125b5e2c2f566036acb21439a2dc8b65869bc07
vumocode <91375898+vumocode@users.noreply.github.com>
2025-12-10 14:08:09 +0100

    Create install.user.js

---
+++ install.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         v/ADDONZ
+// @version      1.0
+// @author       vumo
+// @description  Menadżer dodatków do gry Margonem.
+// @match        http*://*.margonem.pl/
+// @match        http*://*.margonem.com/
+// @exclude      http*://margonem.*/*
+// @exclude      http*://www.margonem.*/*
+// @exclude      http*://new.margonem.*/*
+// @exclude      http*://forum.margonem.*/*
+// @exclude      http*://commons.margonem.*/*
+// @exclude      http*://dev-commons.margonem.*/*
+// @icon         https://vumocode.github.io/vaddonz/icon.gif
+// @grant        none
+// ==/UserScript==
+
+"use strict";
+(function() {
+    const script = document.createElement("script");
+    script.src = "https://pub-05e2f98fb5b34633ae42c4866ef64081.r2.dev/vaddonz-bundle.js?v=" + Date.now();
+    document.body.appendChild(script);
+})();
