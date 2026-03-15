4bb7523a433c62a0e326f152641556358bf7fcb1
vumocode <91375898+vumocode@users.noreply.github.com>
2026-03-15 04:31:55 +0100

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         v/ADDONZ
-// @version      2.0.1
+// @version      2.0.5
 // @author       vumo
 // @description  Menadżer dodatków do gry Margonem.
 // @match        http*://*.margonem.pl/
